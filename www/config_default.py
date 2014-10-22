32e3edf03cfa9d6867e4f906918e3ecc3e9ce98f
Shen <xuchao@u4437e64358a45211fec5.ant.amazon.com>
2014-10-22 10:43:52 +0800

    add config_default.py

--- www/config_default.py
+++ www/config_default.py
@@ -12,7 +12,7 @@ configs = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': '867842',
+        'password': '123456',
         'database': 'it_plans'
     },
     'session': {
